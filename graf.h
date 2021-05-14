7face0ba853c88d363cc3a874cd6b9a60ff9b238
David Bors <davidbors@icloud.com>
2021-05-14 21:30:42 +0300

    added graf

--- graf.h
+++ graf.h
@@ -5,7 +5,50 @@
 #ifndef TEMA3_GRAF_H
 #define TEMA3_GRAF_H
 
+#include "list.h"
+#include "strvector.h"
+
+#ifndef STDLIB_H
+#define STBLIB_H
+#include <stdlib.h>
+#endif
+
 typedef struct graf {
+    int n, m;
+    vector *noduri;
+    mc *muchii;
 } graf;
+
+graf *init_graf()
+{
+    graf *g = (graf*)malloc(sizeof (graf));
+    g->n = 0;
+    g->m = 0;
+    g->noduri = NULL;
+    g->muchii = NULL;
+    return g;
+}
+
+graf *add_node(graf *g, char *node_name)
+{
+    g->n++;
+    g->noduri = append_string(g->noduri, node_name);
+    return g;
+}
+
+graf *add_edge(graf *g, int i, int j)
+{
+    g->m++;
+    g->muchii = append_list(g->muchii, i, j);
+    return g;
+}
+
+void destroy_graf(graf *g)
+{
+    destroy_vector(g->noduri);
+    destroy_list(g->muchii);
+    free(g);
+    g = NULL;
+}
 
 #endif //TEMA3_GRAF_H
---
+++ graf_test.c
@@ -0,0 +1,23 @@
+//
+// Created by David Bors on 14.05.2021.
+//
+#include <stdio.h>
+#include "graf.h"
+
+int main()
+{
+    graf *g = init_graf();
+    g = add_node(g, "actor1");
+    g = add_node(g, "actor2");
+    g = add_node(g, "actor3");
+    g = add_node(g, "actor4");
+    g = add_node(g, "actor5");
+    g = add_edge(g, 0, 1);
+    g = add_edge(g, 0, 2);
+    g = add_edge(g, 0, 3);
+    g = add_edge(g, 1, 3);
+    g = add_edge(g, 1, 5);
+    g = add_edge(g, 4, 5);
+    destroy_graf(g);
+    return 0;
+}
